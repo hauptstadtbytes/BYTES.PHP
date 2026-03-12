02e1fdd503c10c5fd8842acbb9931861851602e5
Sebastian Räse <sebastian.raese@hauptstadtbytes.de>
2026-03-12 16:29:37 +0100

    "for each" loop support added to 'CaseSensitiveArray'

--- src/Primitives/CaseInsensitiveArray.php
+++ src/Primitives/CaseInsensitiveArray.php
@@ -4,14 +4,21 @@
 namespace BytesPhp\Primitives;
 
 //a case-insensitive array
-class CaseInsensitiveArray implements \ArrayAccess
+class CaseInsensitiveArray implements \ArrayAccess, \Iterator
 {
+    //private properties
     private $container = array();
+    private $position;
 
-    public function __construct( Array $initial_array = array() ) {
-        $this->container = array_map( "strtolower", $initial_array );
+    //constructor
+    public function __construct( Array $initialValues = array() ) {
+
+        $this->container = array_map( "strtolower", $initialValues);
+        $this->position = array_key_first($this->container);
+
     }
 
+    //method(s) implementing 'ArrayAccess'
     public function offsetSet($offset, $value): void {
 
         if( is_string( $offset ) ) $offset = strtolower($offset);
@@ -45,6 +52,38 @@ class CaseInsensitiveArray implements \ArrayAccess
             ? $this->container[$offset]
             : null;
 
+    }
+
+    //method(s) implementing 'Iterator'
+    public function rewind(): void {
+        reset($this->container);
+    }
+
+    #[\ReturnTypeWillChange]
+    public function current() {
+        return current($this->container);;
+    }
+
+    #[\ReturnTypeWillChange]
+    public function key() {
+        return key($this->container);
+    }
+
+    public function next(): void {
+        next($this->container);
+    }
+
+    public function valid(): bool {
+        return key($this->container) !== null;
+    }
+
+    //public method(s)
+    public function Keys(): array{
+        return array_keys($this->container);
+    }
+
+    public function Values(): array{
+        return array_values($this->container);
     }
 
 }
--- tests/Primitives/Test-CaseInsensitiveArray.php
+++ tests/Primitives/Test-CaseInsensitiveArray.php
@@ -17,10 +17,27 @@ $array["say"] = "Hello!";
 $array["answer"] = 42;
 $array[42] = "is the answer";
 
-echo($array["say"]."<br \>\n");
-echo($array["SAY"]."<br \>\n");
+echo("<h3>Echo values</h3>");
+echo("say: ".$array["say"]."<br \>\n");
+echo("SAY: ".$array["SAY"]."<br \>\n");
 echo($array["answer"]." ".$array[42]."<br \>\n");
 
+echo("<h3>Modify the 'say' key</h3>");
 $array["say"] = "Bye, bye!";
 echo($array["say"]."<br \>\n");
+
+echo("<h3>Loop for each key value pair</h3>");
+foreach($array as $key => $val) {
+    echo($key.":".$val."<br \>\n");
+}
+
+echo("<h3>Loop for each key</h3>");
+foreach($array->Keys() as $key) {
+    echo($key."<br \>\n");
+}
+
+echo("<h3>Loop for each value</h3>");
+foreach($array->Values() as $val) {
+    echo($val."<br \>\n");
+}
 ?>
